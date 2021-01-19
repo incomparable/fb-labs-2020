3898ad22ccc42e79949d8eaa9cb5a84e47d61964
Natalia Volkova <natali.volkova001@gmail.com>
2021-01-19 13:55:21 +0200

    code changed

--- cp_3/Kaznacheieva_Misniankin_fb81_cp3/PythonApplication1/PythonApplication1.py
+++ cp_3/Kaznacheieva_Misniankin_fb81_cp3/PythonApplication1/PythonApplication1.py
@@ -7,6 +7,14 @@ alpha = ('а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л',
 
 most_used_bigr = ('ст', 'но', 'то', 'на')
 
+def ord(letter):
+    if letter in alpha:
+        #print(alpha.index(letter))
+        return alpha.index(letter)
+def chr(index):
+    if index < len(alpha):
+        #print(alpha[index])
+        return alpha[index]
 
 def count_bigr(filename): #cчитаем биграммы и выводим топ 5 
     fin = open (filename, 'rt', encoding='utf-8')
@@ -23,113 +31,47 @@ def count_bigr(filename): #cчитаем биграммы и выводим то
     return l
 
 
-def ord(let):
-    if let == 'а': return 0
-    if let == 'б': return 1
-    if let == 'в': return 2
-    if let == 'г': return 3
-    if let == 'д': return 4
-    if let == 'е': return 5
-    if let == 'ж': return 6
-    if let == 'з': return 7
-    if let == 'и': return 8
-    if let == 'й': return 9
-    if let == 'к': return 10
-    if let == 'л': return 11
-    if let == 'м': return 12
-    if let == 'н': return 13
-    if let == 'о': return 14
-    if let == 'п': return 15
-    if let == 'р': return 16
-    if let == 'с': return 17
-    if let == 'т': return 18
-    if let == 'у': return 19
-    if let == 'ф': return 20
-    if let == 'х': return 21
-    if let == 'ц': return 22
-    if let == 'ч': return 23
-    if let == 'ш': return 24
-    if let == 'щ': return 25
-    if let == 'ь': return 26
-    if let == 'ы': return 27
-    if let == 'э': return 28
-    if let == 'ю': return 29
-    if let == 'я': return 30
-
-def chr (num):
-    if num == 0: return 'а'
-    if num == 1: return 'б' 
-    if num == 2: return 'в'
-    if num == 3: return 'г' 
-    if num == 4: return 'д' 
-    if num == 5: return 'е' 
-    if num == 6: return 'ж' 
-    if num == 7: return 'з' 
-    if num == 8: return 'и'
-    if num == 9: return 'й' 
-    if num == 10: return 'к' 
-    if num == 11: return 'л' 
-    if num == 12: return 'м' 
-    if num == 13: return 'н' 
-    if num == 14: return 'о' 
-    if num == 15: return 'п'
-    if num == 16: return 'р'
-    if num == 17: return 'с'
-    if num == 18: return 'т'
-    if num == 19: return 'у'
-    if num == 20: return 'ф'
-    if num == 21: return 'х'
-    if num == 22: return 'ц'
-    if num == 23: return 'ч'
-    if num == 24: return 'ш'
-    if num == 25: return 'щ'
-    if num == 26: return 'ь'
-    if num == 27: return 'ы'
-    if num == 28: return 'э'
-    if num == 29: return 'ю'
-    if num == 30: return 'я'
-
 def crack(x1,x2,y1,y2):
     #y' = (a*x' + b) (mod m^2)
     #y'' = (a*x'' + b) (mod m^2)
     # y' - y'' = a(x'-x'') mod m^2
     # x' - наиболее частая биграмма в языке, х'' -следующая за ней по частоте, аналогично с у' и y'' (только в тексте)
-    #rb1 = (ord(x1[0])-1072)*31+(ord(x1[1])-1072)
-    #rb2 = (ord(x2[0])-1072)*31+(ord(x2[1])-1072)
-    #cb1 = (ord(y1[0])-1072)*31+(ord(y1[1])-1072)
-    #cb2 = (ord(y2[0])-1072)*31+(ord(y2[1])-1072)
     rb1 = (ord(x1[0]))*31+ord(x1[1])
     rb2 = (ord(x2[0]))*31+(ord(x2[1]))
     cb1 = (ord(y1[0]))*31+(ord(y1[1]))
     cb2 = (ord(y2[0]))*31+(ord(y2[1]))
-
-    n1 = (rb1 - rb2)% (31*31)
-    n2 = (cb1 - cb2)% (31*31)
-    d = gcd(n1,(31*31))
-    #print(d)
-    n3 = n2/d
-    invers = inv(n1, (31*31)) #a
+    #print(rb1, rb2, cb1, cb2)
+    n1 = (rb1 - rb2)% (31*31)#a
+    n2 = (cb1 - cb2)% (31*31)#b
+    n = 31*31
+    d=gcd(n1,n)
     if d == 1:
-        key= [invers, (cb1-invers*rb1) % (31*31)]
-        print(key)
+        a = (inv(n1, n) * n2) % n
+        key= [a, (cb1-a*rb1) % n]
+        #print(key)
         return key
-    if d!= 1:
-        if d > 1:
+    if d > 1:
+        if n2 % d != 0:
             return 0
-        elif n2 % d == 0:
-            while True:
+        if n2 % d == 0:
+            n3 = n2/d
+            while 1:
                 if(n2 % n1 == 0):
                     x = n2 / n1
                     break
                 n2 += n3
-
             keys=[]
-            print('Let`s create array of keys...')
             for i in range(d):
-                key=[x + n3*i, (cb1-((x + n3*d)*rb1) % 961)]
+                x_0 = x + n3*i
+                x_1 = (cb1-((x + n3*d)*rb1)) % n
+                key=[x_0, x_1]
                 keys.append(key)
-            print(keys)
-            return keys
+
+        #print(keys)
+        return keys
+   
+   
+    
 
 def f(l): #массив без повторений
     n = []
@@ -152,9 +94,13 @@ def find_key(file): # a = key1, b=key2, n=31
         for j in most_used_bigr:
             for k in t:
                 for l in t:
-                    key = crack(i,j,k,l)
-                    lst.append(key)
-   #print(f(lst))
+                    if i != j:
+                        if k != l:
+                            key = crack(i,j,k,l)
+                            lst.append(key)
+                        else: continue
+                    else: continue
+    print(f(lst))
 def decrypt(filename, key1, key2):
     fin=open(filename,'rt', encoding='utf-8')
     fout= open('result.txt', 'wt', encoding='utf-8')
@@ -162,8 +108,8 @@ def decrypt(filename, key1, key2):
     new_arr = [text[i:i+2] for i in range(0, len(text),2)]
     new_text=''
     for bigr in new_arr:
-         first = (((inv(key1, 961) * (( ord(bigr[0]) * 31) + ord(bigr[1]) - key2)) % 961) // 31) 
-         second = ((inv(key1, 961) * (( ord(bigr[0]) * 31) + ord(bigr[1]) - key2)) % 961) % 31 
+         first = (((inv(key1, 961) * (( (ord(bigr[0])) * 31) + (ord(bigr[1])) - key2)) % 961) // 31) 
+         second = ((inv(key1, 961) * (( (ord(bigr[0])) * 31) + (ord(bigr[1])) - key2)) % 961) % 31 
          new_bigramm = chr(first) + chr(second)
          new_text +=new_bigramm
          fout.write(new_bigramm)
@@ -197,25 +143,25 @@ def inv(first, second):
 
 
 def auto_check(filename):
-    alpha = ('о', 'а','е','щ','ф','ь')
+    alpha1 = ('о', 'а','е','щ','ф','ь')
     let_count = 0
     fin=open(filename,'rt', encoding='utf-8')
     text = fin.read()
     for let in text:
                 let_count = let_count + 1 
     temp_count = 0
-    for c in alpha:
-        if c == 'o' and text.count(c)*100*2/let_count < 10:
+    for c in alpha1:
+        if c == 'o' and text.count(c)*100/let_count < 10:
             break
-        if c == 'a' and text.count(c)*100*2/let_count < 5:
+        if c == 'a' and text.count(c)*100/let_count < 5:
             break
-        if c == 'e' and text.count(c)*100*2/let_count < 5:
+        if c == 'e' and text.count(c)*100/let_count < 5:
             break
-        if c == 'ф' and text.count(c)*100*2/let_count < 1:
+        if c == 'ф' and text.count(c)*100/let_count < 1:
             break
-        if c == 'щ' and text.count(c)*100*2/let_count < 1:
+        if c == 'щ' and text.count(c)*100/let_count < 1:
             break
-        if c == 'ь' and text.count(c)*100*2/let_count < 1:
+        if c == 'ь' and text.count(c)*100/let_count < 1:
             break
         print('This text was decrypted in the right way! Congratulations!')
  
@@ -225,10 +171,13 @@ def auto_check(filename):
 
 
 
-count_bigr('07.txt')
+#count_bigr('07.txt')
+#ord('я')
 #inv(5, 21)
-#find_key('07.txt')
-#crack('ст','но', 'вм', 'мь')
-#decrypt ('07.txt', 200, 900)
+#chr(1)
+#find_key('text.txt')
+#crack('ст','но', 'лл', 'цл')
+#decrypt ('text.txt', 200, 900)
 #inv (12, 5)
-auto_check('result.txt')
+#auto_check('result.txt')
+
